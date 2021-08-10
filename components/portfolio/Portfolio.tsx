fecf2bbf9613c59afd7aae19f8e5c759ff7d7749
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-08-10 19:09:36 +0100

    portfolio title

--- components/portfolio/Portfolio.tsx
+++ components/portfolio/Portfolio.tsx
@@ -9,33 +9,37 @@ import Image from 'next/image';
 const Portfolio = () => {
     return (
         <>
-        {/* <h1 className={styles.title}>Projects</h1> */}
         <section className={styles.portfolio} id="projects">
-            {
-                PortfolioData.map((portfolioItem) => (
-                    <div className={styles.gridItem} key={portfolioItem.id}>
-                        <div className={styles.portfolioContent}>
-                            <div className={styles.portfolioImage}>
-                                <Image src={portfolioItem.Image} alt="portfolio" />
-                                <ul>
-                                    <li>
-                                        <a href={portfolioItem.Githublink} target="_blank" rel="noreferrer">
-                                            <GitHubIcon />
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href={portfolioItem.Livepreview} target="_blank" rel="noreferrer">
-                                            <LanguageIcon />
-                                        </a>
-                                    </li>
-                                </ul>
+            <div className={styles.title}>
+                <h1>My Projects</h1>
+            </div>
+            <div className={styles.container}>
+                {
+                    PortfolioData.map((portfolioItem) => (
+                        <div className={styles.gridItem} key={portfolioItem.id}>
+                            <div className={styles.portfolioContent}>
+                                <div className={styles.portfolioImage}>
+                                    <Image src={portfolioItem.Image} alt="portfolio" />
+                                    <ul>
+                                        <li>
+                                            <a href={portfolioItem.Githublink} target="_blank" rel="noreferrer">
+                                                <GitHubIcon />
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href={portfolioItem.Livepreview} target="_blank" rel="noreferrer">
+                                                <LanguageIcon />
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                    <h6>{portfolioItem.Title}</h6>
+                                    <p>{portfolioItem.Text}</p>
                             </div>
-                                <h6>{portfolioItem.Title}</h6>
-                                <p>{portfolioItem.Text}</p>
                         </div>
-                    </div>
-                ))
-            }
+                    ))
+                }
+            </div>
         </section>
         </>
     )
