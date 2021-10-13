a464d43012a11df3c04dc5b7e9a488b7e37231b5
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-13 06:21:11 +0100

    adding scroll anim with react scroll

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,46 +1,57 @@
 import styles from "./navbar.module.scss";
-import { useState } from 'react'
+import { useState } from "react";
+// import Link from "next/Link";
+import { Link } from 'react-scroll'
 
 interface Props {
-    isScrolling: number;
+  isScrolling: number;
 }
 
-
 const Navbar = ({ isScrolling }: Props) => {
-    const [showLinks, setShowLinks] = useState(true)
+  const [showLinks, setShowLinks] = useState(true);
 
-    const toTheTop = () => {
-        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-    };
+  const toTheTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
 
-    return (
-        <header className={`${styles.header} ${isScrolling > 20 ? `${styles.scrolling}` : null }`}>
-            <div className={styles.logo} onClick={toTheTop}>
-                Samuel Solomon
-            </div>
+  return (
+    <header
+      id='home'
+      className={`${styles.header} ${
+        isScrolling > 20 ? `${styles.scrolling}` : null
+      }`}
+    >
+      <div className={styles.logo} onClick={toTheTop}>
+        Samuel Solomon
+      </div>
 
-            <div className={styles.hamburger} onClick={() => setShowLinks(!showLinks)}>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
+      <div
+        className={styles.hamburger}
+        onClick={() => setShowLinks(!showLinks)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
 
-            <nav className={styles.navbar} id={showLinks ? `${styles.hidden}` : ""}>
-                <a className={styles.link} href="/" >
-                    Home
-                </a>
-                <a className={styles.link} href="#skills">
-                    Skills
-                </a>
-                <a className={styles.link} href="#about">
-                    About
-                </a>
-                <a className={styles.link} href="#projects">
-                    Projects
-                </a>
-            </nav>
-        </header>
-    );
+      <nav className={styles.navbar} id={showLinks ? `${styles.hidden}` : ""}>
+        <Link to='/' smooth={true} duration={100}>
+          <a className={styles.link} onClick={toTheTop}>
+            Home
+          </a>
+        </Link>
+        <Link to='skills' smooth={true} duration={100}>
+          <a className={styles.link}>Skills</a>
+        </Link>
+        <Link to='about' smooth={true} duration={100}>
+          <a className={styles.link}>About</a>
+        </Link>
+        <Link to='projects' smooth={true} duration={100}>
+          <a className={styles.link}>Projects</a>
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
