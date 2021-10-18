e0ebfa3c3ba6a5b2cf81606be44aac6a3bdca3b0
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-19 00:22:12 +0100

    removing dummy data

--- data/PortfolioData.js
+++ data/PortfolioData.js
@@ -1,62 +1,46 @@
 import img1 from '../public/img1.jpg'
 import img2 from '../public/img2.jpg'
 import img3 from '../public/img3.jpg'
+// import appmodern from '../public/screenshots/appmodern.png'
 
 const PortfolioData = [
     {
         id: 1,
         Image: img1,
-        Githublink: 'https://github.com/S-Solomon',
-        Livepreview: 'www.google.com',
-        Title: 'Project 1',
-        Text: 'Text for the first project goes here',
-        category: 'React'
+        Githublink: 'https://github.com/S-Solomon/App-Modern',
+        Livepreview: 'https://app-modern-woad.vercel.app/',
+        Title: 'App Modern',
+        Text: 'Typescript | React | styled-components | React Testing Library',
+        category: 'typescript'
     },
     {
         id: 2,
-        Image: img2,
-        Githublink: 'https://github.com/S-Solomon',
-        Livepreview: 'www.google.com',
-        Title: 'Project 2',
-        Text: 'Text for the second project goes here',
-        category: 'Typescript'
+        Image: img1,
+        Githublink: 'https://github.com/S-Solomon/ReactJSPortfolio-v2',
+        Livepreview: 'https://react-js-portfolio-v2.vercel.app/',
+        Title: 'Portfolio Website',
+        Text: 'Javascript | Sass | React',
+        category: 'Javascript'
     },
     {
         id: 3,
-        Image: img3,
-        Githublink: 'https://github.com/S-Solomon',
-        Livepreview: 'www.google.com',
-        Title: 'Project 3',
-        Text: 'Text for the third project goes here',
-        category: 'React'
+        Image: img1,
+        Githublink: 'https://github.com/S-Solomon/Covid19-Stats',
+        Livepreview: 'https://covid19-stats-delta.vercel.app/',
+        Title: 'Covid-19 Stats',
+        Text: 'Typescript | API | Styled-Components',
+        category: 'Typescript'
     },
     {
         id: 4,
-        Image: img3,
+        Image: img1,
         Githublink: 'https://github.com/S-Solomon',
         Livepreview: 'www.google.com',
-        Title: 'Project 3',
-        Text: 'Text for the third project goes here',
-        category: 'React'
+        Title: 'SamuelSolomon.io',
+        Text: 'NextJs | Typescript | Sass',
+        category: 'Typescript nextjs'
     },
-    {
-        id: 5,
-        Image: img3,
-        Githublink: 'https://github.com/S-Solomon',
-        Livepreview: 'www.google.com',
-        Title: 'Project 3',
-        Text: 'Text for the third project goes here',
-        category: 'Python'
-    },
-    {
-        id: 6,
-        Image: img3,
-        Githublink: 'https://github.com/S-Solomon',
-        Livepreview: 'www.google.com',
-        Title: 'Project 3',
-        Text: 'Text for the third project goes here',
-        category: 'Next Js'
-    }
+    
 ]
 
 export default PortfolioData
