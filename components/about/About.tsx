fe73474911324a7f2aa6c8e3a36445ef6fed5d7e
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-21 06:57:08 +0100

    download cv functionality

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -2,29 +2,27 @@
 import React, { ReactElement } from 'react';
 import styles from './about.module.scss'
 
-
 const About = (): ReactElement => {
     return (
-      <div className={styles.container} id='about'>
-        <div className={styles.description}>
-          <h1>Hi, I’m Samuel. Nice to meet you.</h1>
-          <p>
-            I&apos;m a highly motivated self-taught developer, seeking to launch a career building and maintaining
-            web applications and services.
-             
-          </p>
-          <br />
-          <p></p>
+        <div className={styles.container} id="about">
+            <div className={styles.description}>
+                <h1>Hi, I’m Samuel. Nice to meet you.</h1>
+                <p>
+                    I&apos;m a highly motivated self-taught developer, seeking
+                    to launch a career building and maintaining web applications
+                    and services.
+                </p>
+                <br />
+                <a
+                    className={styles.cv}
+                    href="/files/Resume.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Download cv
+                </a>
+            </div>
         </div>
-        {/* <div className={styles.wrapper}>
-          <Image
-            src='/svgRemix.svg'
-            width={450}
-            height={250}
-            alt='picture of the author'
-          />
-        </div> */}
-      </div>
     );
 }
 
