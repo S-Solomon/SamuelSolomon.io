d6fbce70c7981178c647aa09b0dc969fe641fe33
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-31 12:38:34 +0000

    adding express tracker application to portfolio

--- data/PortfolioData.js
+++ data/PortfolioData.js
@@ -1,46 +1,56 @@
 import appmodern from '../public/screenshots/appmodern.png'
 import portfoliowebsite from '../public/screenshots/portfoliowebsite.png'
+import expensetracker from '../public/screenshots/expensetracker.png'
 import covid19 from '../public/screenshots/covid-19.png'
 import samuelsolomon from '../public/screenshots/samuelsolomon.io.png'
+
 
 const PortfolioData = [
     {
         id: 1,
-        Image: appmodern,
-        Githublink: 'https://github.com/S-Solomon/App-Modern',
-        Livepreview: 'https://app-modern-woad.vercel.app/',
-        Title: 'App Modern',
-        Text: 'Typescript | React | styled-components | React Testing Library',
-        category: 'typescript'
+        Image: expensetracker,
+        Githublink: "https://github.com/S-Solomon/Expense-Tracker",
+        Livepreview: "https://expensetrackerapp.vercel.app/",
+        Title: "Expense Tracker App",
+        Text: "React | Css |  ",
+        category: "javascript",
     },
     {
         id: 2,
-        Image: portfoliowebsite,
-        Githublink: 'https://github.com/S-Solomon/ReactJSPortfolio-v2',
-        Livepreview: 'https://react-js-portfolio-v2.vercel.app/',
-        Title: 'Portfolio Website',
-        Text: 'Javascript | Sass | React',
-        category: 'Javascript'
+        Image: appmodern,
+        Githublink: "https://github.com/S-Solomon/App-Modern",
+        Livepreview: "https://app-modern-woad.vercel.app/",
+        Title: "App Modern",
+        Text: "Typescript | React | styled-components | React Testing Library",
+        category: "typescript",
     },
     {
         id: 3,
-        Image: covid19,
-        Githublink: 'https://github.com/S-Solomon/Covid19-Stats',
-        Livepreview: 'https://covid19-stats-delta.vercel.app/',
-        Title: 'Covid-19 Stats',
-        Text: 'Typescript | API | Styled-Components | React',
-        category: 'Typescript'
+        Image: portfoliowebsite,
+        Githublink: "https://github.com/S-Solomon/ReactJSPortfolio-v2",
+        Livepreview: "https://react-js-portfolio-v2.vercel.app/",
+        Title: "Portfolio Website",
+        Text: "Javascript | Sass | React",
+        category: "Javascript",
     },
     {
         id: 4,
-        Image: samuelsolomon,
-        Githublink: 'https://github.com/S-Solomon/SamuelSolomon.io',
-        Livepreview: 'https://www.samuelsolomon.io',
-        Title: 'SamuelSolomon.io',
-        Text: 'NextJs | Typescript | Sass',
-        category: 'Typescript nextjs'
+        Image: covid19,
+        Githublink: "https://github.com/S-Solomon/Covid19-Stats",
+        Livepreview: "https://covid19-stats-delta.vercel.app/",
+        Title: "Covid-19 Stats",
+        Text: "Typescript | API | Styled-Components | React ",
+        category: "Typescript",
     },
-    
-]
+    // {
+    //     id: 5,
+    //     Image: samuelsolomon,
+    //     Githublink: 'https://github.com/S-Solomon/SamuelSolomon.io',
+    //     Livepreview: 'https://www.samuelsolomon.io',
+    //     Title: 'SamuelSolomon.io',
+    //     Text: 'NextJs | Typescript | Sass',
+    //     category: 'Typescript nextjs'
+    // },
+];
 
 export default PortfolioData
