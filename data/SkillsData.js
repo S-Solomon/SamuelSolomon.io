59b211354204e1c8e71ec40a04a9c49a1c18ed6c
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-11-02 18:18:18 +0000

    new skill: webpack

--- data/SkillsData.js
+++ data/SkillsData.js
@@ -9,85 +9,85 @@ import Github from '../public/skills/GitHub.png'
 import Jest from '../public/skills/jest.svg'
 import ReactTestingLibrary from '../public/skills/octopus.png'
 import styledComponents from '../public/skills/styled-components.png'
-import Figma from '../public/skills/Figma.png'
+import Webpack from '../public/skills/webpack.svg'
 
 import { nanoid } from 'nanoid'
 
 
-const SkillsData =  [
+const SkillsData = [
     {
         id: nanoid(),
         image: Html,
-        title: 'HTML5',
-        description: 'html logo'
+        title: "HTML5",
+        description: "html logo",
     },
     {
         id: nanoid(),
         image: Css,
-        title: 'CSS3',
-        description: 'css logo'
+        title: "CSS3",
+        description: "css logo",
     },
     {
         id: nanoid(),
         image: Javascript,
-        title: 'Javascript(ES6)',
-        description: 'javascript logo'
+        title: "Javascript(ES6)",
+        description: "javascript logo",
     },
     {
         id: 4,
         image: React,
-        title: 'React',
-        description: 'react logo'
+        title: "React",
+        description: "react logo",
     },
     {
         id: nanoid(),
         image: Sass,
-        title: 'Scss',
-        description: 'sass logo'
+        title: "Scss",
+        description: "sass logo",
     },
     {
         id: nanoid(),
         image: Typescript,
-        title: 'Typescript',
-        description: 'Typescript logo'
+        title: "Typescript",
+        description: "Typescript logo",
     },
     {
         id: nanoid(),
         image: Git,
-        title: 'Git',
-        description: 'Git logo'
+        title: "Git",
+        description: "Git logo",
     },
     {
         id: nanoid(),
         image: Jest,
-        title: 'Jest',
-        description: 'Jest logo'
+        title: "Jest",
+        description: "Jest logo",
     },
     {
         id: nanoid(),
         image: Github,
-        title: 'Github',
-        description: 'Github logo'
+        title: "Github",
+        description: "Github logo",
     },
     {
         id: nanoid(),
         image: ReactTestingLibrary,
-        title: 'Testing Library',
-        description: 'ReactTestingLibrary'
+        title: "Testing Library",
+        description: "ReactTestingLibrary",
     },
     {
         id: nanoid(),
         image: styledComponents,
-        title: 'Styled Components',
-        description: 'styled components'
+        title: "Styled Components",
+        description: "styled components",
     },
     {
         id: nanoid(),
-        image: Figma,
-        title: 'Figma',
-        description: 'Figma'
-    }
-]
+        image: Webpack,
+        title: "Webpack",
+        description: "Webpack",
+    },
+];
 
 
 
